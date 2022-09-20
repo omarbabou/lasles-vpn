53f55b3a8b45b98b4922853f87d9c4e691b8cbd6
omarbabou <obabou50@gmail.com>
2022-09-20 17:00:48 +0200

    Add icons section

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
+/* eslint-disable indent */
 import React from 'react';
 import IconLaslesVpn from './graphics/Logo.png';
 import Illustration1 from './graphics/Illustration 1.svg';
+import IconUsers from './graphics/users.svg';
+import IconMaps from './graphics/maps.svg';
+import IconServers from './graphics/servers.svg';
 
 function App() {
   const menus = ['About', 'Features', 'Pricing', 'Testimonials', 'Help'];
+  const section3 = [
+    {
+      icons: IconUsers,
+      label: 'Users',
+      total: '90+',
+    },
+    {
+      icons: IconMaps,
+      label: 'Locations',
+      total: '30+',
+    },
+    {
+      icons: IconServers,
+      label: 'Servers',
+      total: '50+',
+    },
+  ];
+
   return (
     <div className="bg-white">
       <header className="container max-w-5xl mx-auto flex flex-row pt-12 items-center space-x-36">
@@ -22,21 +44,41 @@ function App() {
         </div>
       </header>
 
-      <main className="container max-w-5xl mx-auto grid grid-cols-2 py-24">
-        <div>
-          <h1 className="font-bold text-4xl pb-5">
-            Want anything to be easy
-            <br />
-            With LaslesVPN.
-          </h1>
-          <div className="font-normal text-xs pb-12">
-            Provide a network for all your needs with
-            ease and fun using LaslesVPN discover interesting features from us.
+      <main className="container max-w-5xl mx-auto">
+        <div className="grid grid-cols-2 py-24">
+          <div>
+            <h1 className="font-bold text-4xl pb-5">
+              Want anything to be easy
+              <br />
+              With LaslesVPN.
+            </h1>
+            <div className="font-normal text-xs pb-12">
+              Provide a network for all your needs with
+              ease and fun using LaslesVPN discover interesting features from us.
+            </div>
+            <button type="submit" className="py-4 px-16 bg-red-500 rounded-md text-white drop-shadow-3xl">Get started</button>
           </div>
-          <button type="submit" className="py-4 px-16 bg-red-500 rounded-md text-white drop-shadow-3xl">Get started</button>
+          <div>
+            <img src={Illustration1} alt="illustration-laslesvpn" />
+          </div>
         </div>
-        <div>
-          <img src={Illustration1} alt="illustration-laslesvpn" />
+        <div className="grid grid-cols-3 shadow-2xl rounded-md py-4">
+          {section3.map((val, index) => (
+            <div
+              // eslint-disable-next-line react/no-array-index-key
+              key={index}
+              className={`flex flex-row py-4 space-x-6 justify-center ${
+            index + 1 !== section3.length && 'border-r border-gray-200'}`}
+            >
+              <div className="rounded-full bg-red-100 p-3">
+                <img alt={val.label} src={val.icons} className="w-6 h-6" />
+              </div>
+              <div>
+                <div>{val.total}</div>
+                <div>{val.label}</div>
+              </div>
+            </div>
+            ))}
         </div>
       </main>
     </div>
